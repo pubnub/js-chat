9cf8aa3cd26ff7e1b5ebd071eb5aa2989604b614
anton-pub <antoni.malinowski@pubnub.com>
2023-03-29 20:59:06 +0200

    Pipeline fix. Add afterAll

--- lib/tests/message.test.ts
+++ lib/tests/message.test.ts
@@ -1,6 +1,7 @@
 import { Chat, Channel } from "../src"
 
 describe("Send message test", () => {
+  afterAll(() => setTimeout(() => process.exit(), 1000))
   test("should verify if message sent", async () => {
     const chat = Chat.init({
       publishKey: "demo",
@@ -9,22 +10,22 @@ describe("Send message test", () => {
     })
 
     const messages = []
-
     const response = await chat.getChannel("test-react-channel-C1")
-    let recieveTime = 0
+    let receiveTime = 0
+
     response.connect((message) => {
-      recieveTime = Date.now()
+      receiveTime = Date.now()
       messages.push(message.content)
     })
 
     const sendTime = Date.now()
-    response.sendText("message")
+    await response.sendText("message")
 
     const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
     await sleep(2000)
 
-    const elapsedTime = recieveTime - sendTime
+    const elapsedTime = receiveTime - sendTime
     console.log(elapsedTime)
-    expect(elapsedTime).toBeLessThan(400)
+    expect(elapsedTime).toBeLessThan(500)
   })
 })
