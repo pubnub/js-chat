a3064c0ffa9a158c864d14eece13f5449e3da82e
piotr-suwala <112620304+piotr-suwala@users.noreply.github.com>
2023-06-14 11:57:02 +0200

    Csk 100 suggestions core (#65)
    * feat(lib): add channel mentions
    
    * feat(lib): remove logs
    
    * feat(lib): add mentioned users to metadata
    
    * feat(lib): feedback after CR
    
    * feat(lib): feedback after CR
    
    * feat(lib): mentioned users wip
    
    * feat(lib): fix a test
    
    * feat(lib): add channel and global user suggestions
    
    * feat(lib): remove unused imports

--- samples/angular/src/chat-sdk/message-input/message-input.component.ts
+++ samples/angular/src/chat-sdk/message-input/message-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core"
-import { Channel } from "@pubnub/chat"
+import { Channel, Chat, User } from "@pubnub/chat"
 
 @Component({
   selector: "app-message-input-chat",
@@ -8,11 +8,33 @@ import { Channel } from "@pubnub/chat"
 })
 export class MessageInputComponentChat {
   pubnubInput = ""
+  suggestedUsers: User[] = []
+  usersToNotify: User[] = []
   @Input() channel!: Channel
+  @Input() chat!: Chat
   @Input() typingSent!: boolean
 
-  handleInput() {
+  async handleInput(text: string) {
+    const result = await this.chat.getSuggestedGlobalUsers(text)
+    console.log("result", result)
+    this.suggestedUsers = result
+    // const members = await this.channel.getSuggestedChannelMembers(text)
+    // console.log("members", members)
     this.pubnubInput ? this.channel.startTyping() : this.channel.stopTyping()
+  }
+
+  toggleUserToNotify(user: User) {
+    const userAlreadyAdded = this.usersToNotify.find((u) => u.id === user.id)
+
+    if (!!userAlreadyAdded) {
+      this.usersToNotify = this.usersToNotify.filter((u) => u.id !== user.id)
+    } else {
+      this.usersToNotify.push(user)
+    }
+  }
+
+  isUserToBeNotified(user: User) {
+    return !!this.usersToNotify.find((u) => u.id === user.id)
   }
 
   async handleSend() {
