68a6723e9c5f6c6477df6048f59de424a42a967a
Piotr Suwala <piotr.suwala@pubnub.com>
2023-06-20 15:15:19 +0200

    feat(lib): add user mentions

--- lib/src/entities/messageToSend.ts
+++ lib/src/entities/messageToSend.ts
@@ -1,19 +1,16 @@
-import { Chat } from "./chat";
-import { User } from "./user";
+import { Chat } from "./chat"
+import { User } from "./user"
 
 declare global {
   interface Array<T> {
-    findLastIndex(
-      predicate: (value: T, index: number, obj: T[]) => unknown,
-      thisArg?: any
-    ): number
+    findLastIndex(predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: any): number
   }
 }
 
 export class MessageToSend {
   private chat: Chat
-  public value: string = ""
-  private previousValue: string = ""
+  public value = ""
+  private previousValue = ""
   private mentionedUsers: {
     [nameOccurrenceIndex: number]: User
   } = {}
@@ -26,8 +23,10 @@ export class MessageToSend {
     this.previousValue = this.value
     this.value = text
 
-    let previousWordsStartingWithAt = this.previousValue.split(" ").filter(word => word.startsWith("@"))
-    let currentWordsStartingWithAt = this.value.split(" ").filter(word => word.startsWith("@"))
+    const previousWordsStartingWithAt = this.previousValue
+      .split(" ")
+      .filter((word) => word.startsWith("@"))
+    const currentWordsStartingWithAt = this.value.split(" ").filter((word) => word.startsWith("@"))
     let differentMentionPosition = -1
 
     const differentMentions = currentWordsStartingWithAt.filter((m, i) => {
@@ -38,7 +37,7 @@ export class MessageToSend {
       }
 
       return previousWordsStartingWithAt.indexOf(m) === -1
-    });
+    })
 
     Object.keys(this.mentionedUsers).forEach((key) => {
       const mentionedUserName = this.mentionedUsers[Number(key)]?.name
@@ -46,24 +45,34 @@ export class MessageToSend {
       if (mentionedUserName && !currentWordsStartingWithAt[Number(key)]) {
         delete this.mentionedUsers[Number(key)]
       }
+
+      const splitMentionsByAt = (this.value.match(/(?<=^|\s)@[^@\s]+(?:\s+[^@\s]+)*/g) || []).map(
+        (splitMention) => splitMention.substring(1)
+      )
+
+      if (mentionedUserName && !splitMentionsByAt[Number(key)]?.startsWith(mentionedUserName)) {
+        delete this.mentionedUsers[Number(key)]
+      }
     })
 
-    const differentMention = differentMentions.length ? {
-      name: differentMentions[0],
-      nameOccurrenceIndex: differentMentionPosition,
-    } : null
+    const differentMention = differentMentions.length
+      ? {
+          name: differentMentions[0],
+          nameOccurrenceIndex: differentMentionPosition,
+        }
+      : null
 
     return {
       differentMention,
     }
   }
 
-  addMentionedUser(user: User, mention: { name: string, nameOccurrenceIndex: number }) {
-    let counter = 0;
+  addMentionedUser(user: User, mention: { name: string; nameOccurrenceIndex: number }) {
+    let counter = 0
     let result = ""
     let isUserFound = false
 
-    this.value.split(" ").forEach(word => {
+    this.value.split(" ").forEach((word) => {
       if (!word.startsWith("@")) {
         result += `${word} `
       } else {
@@ -88,21 +97,30 @@ export class MessageToSend {
   getPayloadToSend() {
     return {
       text: this.value,
-      mentionedUsers: Object.keys(this.mentionedUsers).reduce((acc, key) => ({ ...acc, [key]: { id: this.mentionedUsers[Number(key)].id, name: this.mentionedUsers[Number(key)].name } }), {}),
+      mentionedUsers: Object.keys(this.mentionedUsers).reduce(
+        (acc, key) => ({
+          ...acc,
+          [key]: {
+            id: this.mentionedUsers[Number(key)].id,
+            name: this.mentionedUsers[Number(key)].name,
+          },
+        }),
+        {}
+      ),
     }
   }
 
   getHighlightedMention(selectionStart: number) {
     const necessaryText = this.value.slice(0, selectionStart - 1)
-
     const necessaryTextSplitBySpace = necessaryText.split(" ")
-
-    const onlyWordsWithAt = necessaryTextSplitBySpace.filter(word => word.startsWith("@"))
-
-    const lastMentionedUserInTextIndex = necessaryTextSplitBySpace.findLastIndex(word => word.startsWith("@"))
-
-    const lastMentionedUserInText = necessaryTextSplitBySpace.slice(lastMentionedUserInTextIndex, necessaryTextSplitBySpace.length)
-
+    const onlyWordsWithAt = necessaryTextSplitBySpace.filter((word) => word.startsWith("@"))
+    const lastMentionedUserInTextIndex = necessaryTextSplitBySpace.findLastIndex((word) =>
+      word.startsWith("@")
+    )
+    const lastMentionedUserInText = necessaryTextSplitBySpace.slice(
+      lastMentionedUserInTextIndex,
+      necessaryTextSplitBySpace.length
+    )
     const lastMentionedUser = this.mentionedUsers[onlyWordsWithAt.length - 1]
 
     if (!lastMentionedUser?.name) {
