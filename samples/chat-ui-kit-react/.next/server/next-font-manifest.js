7e90851b0da6e6355be0c1680b83514fd6d3a10d
Piotr Suwala <piotr.suwala@pubnub.com>
2023-08-21 15:42:02 +0200

    feat(lib): add channel suggestions

--- samples/chat-ui-kit-react/.next/server/next-font-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__NEXT_FONT_MANIFEST =
-  '{"pages":{},"app":{"/Users/piotr.suwala/pubnub/js-chat/samples/chat-ui-kit-react/src/app/layout":["static/media/2aaf0723e720e8b9-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}'
