66bbff0905ab488b821565354a4a9a90b06b0a67
piotr-suwala <112620304+piotr-suwala@users.noreply.github.com>
2024-06-18 15:19:54 +0200

    [CSK-786] fix(lib): take membership related events into account (#172)
    * fix(lib): take membership related events into account
    
    * fix(lib): save some time on testing
    
    * fix(lib): save some time on testing
    
    * feat(lib): rm unsubscribeAll
    
    * feat(lib): revert tests
    
    * PubNub SDK v0.7.3 release.
    
    ---------
    
    Co-authored-by: PubNub Release Bot <120067856+pubnub-release-bot@users.noreply.github.com>

--- lib/tests/user.test.ts
+++ lib/tests/user.test.ts
@@ -3,8 +3,6 @@ import { createChatInstance, createRandomUser, sleep } from "./utils"
 import { INTERNAL_ADMIN_CHANNEL } from "../src"
 
 describe("User test", () => {
-  jest.retryTimes(3)
-
   let chat: Chat
   let user: User
 
@@ -59,6 +57,64 @@ describe("User test", () => {
 
     expect(callback).toHaveBeenCalledWith(updatedUser)
     expect(updatedUser.name).toEqual(name)
+
+    stopUpdates()
+  })
+
+  test("should update the user even if they're a member of a particular channel", async () => {
+    let someUser = await chat.getUser("test-user-chatsdk0")
+    if (!someUser) {
+      someUser = await chat.createUser("test-user-chatsdk0", { name: "Chat SDK user 0" })
+    }
+    let someChannel = await chat.getChannel("some-public-channel")
+    if (!someChannel) {
+      someChannel = await chat.createPublicConversation({
+        channelId: "some-public-channel",
+        channelData: { name: "Public channel test" },
+      })
+    }
+    await chat.sdk.objects.setChannelMembers({
+      channel: someChannel.id,
+      uuids: [someUser.id],
+    })
+
+    const stopUpdates = User.streamUpdatesOn([someUser], (updatedUsers) => {
+      someUser = updatedUsers[0]
+    })
+    await someUser.update({ name: "update number 1" })
+    await sleep(1000)
+    expect(someUser.name).toBe("update number 1")
+    await someUser.update({ name: "update number 2" })
+    await sleep(1000)
+    expect(someUser.name).toBe("update number 2")
+
+    stopUpdates()
+  })
+
+  test("should update the user even if they're not a member of a particular channel", async () => {
+    let someUser = await chat.getUser("test-user-chatsdk1")
+    if (!someUser) {
+      someUser = await chat.createUser("test-user-chatsdk1", { name: "Chat SDK user 1" })
+    }
+    let someChannel = await chat.getChannel("some-public-channel-2")
+    if (!someChannel) {
+      someChannel = await chat.createPublicConversation({
+        channelId: "some-public-channel-2",
+        channelData: { name: "Public channel test 2" },
+      })
+    }
+    const { members } = await someChannel.getMembers()
+
+    expect(members.length).toBe(0)
+    const stopUpdates = User.streamUpdatesOn([someUser], (updatedUsers) => {
+      someUser = updatedUsers[0]
+    })
+    await someUser.update({ name: "update number 1" })
+    await sleep(1000)
+    expect(someUser.name).toBe("update number 1")
+    await someUser.update({ name: "update number 2" })
+    await sleep(1000)
+    expect(someUser.name).toBe("update number 2")
 
     stopUpdates()
   })
